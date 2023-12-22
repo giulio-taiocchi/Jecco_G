79a3eaf444af67a40052183eeb392b2c3078d74f
giulio-taiocchi <giulio.taiocchi@gmail.com>
2023-12-22 12:29:52 +0000

    added output for he grid

--- src/AdS4_3_1/initial_data.jl
+++ src/AdS4_3_1/initial_data.jl
@@ -105,7 +105,7 @@ function (id::ID_ConstantAH)(bulkconstrains, bulkevols, bulkderivs, boundary::Bo
     
     #printing u
     
-    for nsys in systems.N
+    for nsys in 4
 	    println("domain $nsys")
 	    actual_system = systems[nsys]
 	    Nu, Nx, Ny = size(actual_system)
